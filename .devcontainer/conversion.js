c3aa821ed9db761b0f156218eb4c99fba0d9ddf3
Arpit Mishra <117110049+Arpit9838@users.noreply.github.com>
2023-10-27 15:28:24 +0000

    conversion of datatypes

---
+++ .devcontainer/conversion.js
@@ -0,0 +1,9 @@
+let score = "33"
+console.log(typeof score);
+let valnumber = Number(score)
+console.log(typeof valnumber);
+let isLoggedIn = 1
+let boolean =Boolean(isLoggedIn)
+console.log(boolean)
+//1=true
+//"" =false
