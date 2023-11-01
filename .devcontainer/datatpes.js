7349fcd5b0b3e101c17c75f9cf7242b85fffcff9
Arpit Mishra <117110049+Arpit9838@users.noreply.github.com>
2023-11-01 15:07:19 +0000

    datatpe

---
+++ .devcontainer/datatpes.js
@@ -0,0 +1,19 @@
+//primitive
+const scor=100
+const id=  Symbol('123')
+const  aid = Symbol('123')
+console.log(id===aid);
+//Array ,objects , function
+const heros =["shaktiman","naagraj","doga"];
+//object
+
+let obj =
+    {
+        name:"arpit",
+        age: 22,
+    
+    }
+//function
+const myfun= function(){
+    console.log("hello");
+}
