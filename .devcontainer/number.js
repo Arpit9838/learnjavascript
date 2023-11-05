1953f03712d2d3df232699a4f580a7287b99ae76
Arpit Mishra <117110049+Arpit9838@users.noreply.github.com>
2023-11-05 07:46:50 +0000

    done

---
+++ .devcontainer/number.js
@@ -0,0 +1,20 @@
+const  num = 400.943
+const bal = new Number(100)
+console.log(bal.toString().length);
+console.log(bal.toFixed(2));
+console.log(num.toPrecision(3));
+const nu =100000
+console.log(nu.toLocaleString('en-IN'));
+//************************************** maths*/
+console.log(Math.abs(-2));
+console.log(Math.round(4.7));
+console.log(Math.ceil(2.7));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min =10
+const max=20
+console.log((Math.random()*(max-min+1))+min)
+
+
+
+
